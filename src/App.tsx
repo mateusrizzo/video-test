1e6cfe8340d70d753a156200fd865ed6977add6f
Mateus Gonçalves Rizzo <mateusrizzo13@gmail.com>
2020-08-18 12:29:56 -0300

    quiz card styled

--- src/App.tsx
+++ src/App.tsx
@@ -23,21 +23,25 @@ function App() {
   }
   return (
     <div className="container">
-      <h1 className="page-title">Revolução Francesa</h1>
+      <div className="title-container">
+        <h1 className="page-title">Revolução Francesa</h1>
+      </div>
       <div className="video-container">
         <video controls src={paris} ref={player} onTimeUpdate={handleQuiz}/>
       </div>
-      {/* {!questionVisible ? null : */}
+      {!questionVisible ? null :
         <div className="quiz-container">
           <div className="question">
             <p className="question-label">Quanto tempo durou a revolução francesa?</p>
-            <button className="options">A. 12 anos</button>
-            <button className="options">B. 10 anos</button>
-            <button className="options">C. 20 anos</button>
-            <button className="options">D. 8 anos</button>
+            <div className='buttons-container'>
+              <button className="options">A. 12 anos</button>
+              <button className="options">B. 10 anos</button>
+              <button className="options">C. 20 anos</button>
+              <button className="options">D. 8 anos</button>
+            </div>
           </div>
         </div>
-        {/* } */}
+      }
     </div>
   );
 }
